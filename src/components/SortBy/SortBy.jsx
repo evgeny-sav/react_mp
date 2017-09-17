80ffe787df9ef8a8db1608939d1a3d52f4fffcc4
evgeny-sav <yauheni.web@gmail.com>
2017-09-17 18:32:56 +0300

    infobar styles

--- src/components/SortBy/SortBy.jsx
+++ src/components/SortBy/SortBy.jsx
@@ -7,11 +7,18 @@ class SortBy extends Component {
       <div className={styles.filters}>
         <p className={styles.title}>Sort by: </p>
         <ul className={styles.filtersList}>
-          <li><a href="#" className="text-danger">release date</a></li>
-          <li className={styles.active}><a href="#" className="text-muted">rating</a></li>
+          <li>
+            <a href="#" className="text-danger">
+              release date
+            </a>
+          </li>
+          <li className={styles.active}>
+            <a href="#" className="text-muted">
+              rating
+            </a>
+          </li>
         </ul>
       </div>
-
     );
   }
 }
