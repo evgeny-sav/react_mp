da4be0914e88b5bdfef2c74dbedda3c83df42ddb
Yauheni_Sauchanka <Yauheni_Sauchanka@epam.com>
2017-09-18 13:12:02 +0300

    MovieList:Item markup done

--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -6,13 +6,22 @@ class MovieItem extends Component {
   render() {
     return (
       <div className={styles.movieItem}>
-        <img src={this.props.imageUrl} alt="" />
+        <div className={styles.movieImage}>
+          <img src={this.props.imageUrl} alt="" />
+        </div>
+        <div className={styles.movieInfo}>
+          <p className={styles.title}>
+            {this.props.title} <span className={styles.year}>2017</span>
+          </p>
+          <p className={styles.genre}>Fiction</p>
+        </div>
       </div>
     );
   }
 }
 
 MovieItem.propTypes = {
+  title: PropTypes.string.isRequired,
   imageUrl: PropTypes.string.isRequired,
 };
 
