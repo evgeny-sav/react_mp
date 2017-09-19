b247f0f154707d5f73494086c62da017f013e028
Yauheni_Sauchanka <Yauheni_Sauchanka@epam.com>
2017-09-19 15:16:58 +0300

    propTypes

--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -2,25 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './movie-item.scss';
 
-const MovieItem = () => {
-  this.propTypes = {
-    title: PropTypes.string.isRequired,
-    imageUrl: PropTypes.string.isRequired,
-  };
-
-  return (
-    <div className={styles.movieItem}>
-      <div className={styles.movieImage}>
-        <img src={this.props.imageUrl} alt="" />
-      </div>
-      <div className={styles.movieInfo}>
-        <p className={styles.title}>
-          {this.props.title} <span className={styles.year}>2017</span>
-        </p>
-        <p className={styles.genre}>Fiction</p>
-      </div>
+const MovieItem = ({title, imageUrl}) => (
+  <div className={styles.movieItem}>
+    <div className={styles.movieImage}>
+      <img src={imageUrl} alt="" />
     </div>
-  );
-};
+    <div className={styles.movieInfo}>
+      <p className={styles.title}>
+        {title} <span className={styles.year}>2017</span>
+      </p>
+      <p className={styles.genre}>Fiction</p>
+    </div>
+  </div>
+);
 
+MovieItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  imageUrl: PropTypes.string.isRequired,
+};
 export default MovieItem;
