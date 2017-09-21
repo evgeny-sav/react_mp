9118adc8b11339e39403c58f02aa585b6c1e0860
Yauheni_Sauchanka <Yauheni_Sauchanka@epam.com>
2017-09-21 16:21:48 +0300

    handleSearchBy and handleSortBy

--- src/components/SearchBy/SearchBy.jsx
+++ src/components/SearchBy/SearchBy.jsx
@@ -1,21 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classNames from 'classnames';
 import styles from './searchby.scss';
 
 const cx = classNames.bind(styles);
 
-const SearchBy = () => (
-  <div className={styles.searchBy}>
-    <p className={styles.title}>Search by</p>
-    <ul className={styles.list}>
-      <li className={cx(styles.item, styles.active)}>
-        <a href="#bydirector">Director</a>
-      </li>
-      <li className={styles.item}>
-        <a href="#bytitle">Title</a>
-      </li>
-    </ul>
-  </div>
-);
+class SearchBy extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchBy: 'title',
+    };
+  }
+
+  handelSearch(e, val) {
+    e.preventDefault();
+    switch (val) {
+      case 'title': this.setState({ searchBy: val });
+        break;
+      case 'director': this.setState({ searchBy: val });
+        break;
+      default: this.setState({ searchBy: 'title' });
+    }
+  }
+
+  render() {
+    return (
+      <div className={styles.searchBy}>
+        <p className={styles.title}>Search by</p>
+        <ul className={styles.list}>
+          <li className={cx(styles.item, this.state.searchBy === 'director' ? styles.active : null)}>
+            <a href="" onClick={e => this.handelSearch(e, 'director')}>Director</a>
+          </li>
+          <li className={cx(styles.item, this.state.searchBy === 'title' ? styles.active : null)}>
+            <a href="" onClick={e => this.handelSearch(e, 'title')}>Title</a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default SearchBy;
--- src/components/SortBy/SortBy.jsx
+++ src/components/SortBy/SortBy.jsx
@@ -1,22 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './sortby.scss';
 
-const SortBy = () => (
-  <div className={styles.filters}>
-    <p className={styles.title}>Sort by: </p>
-    <ul className={styles.filtersList}>
-      <li>
-        <a href="#byrelease" className="text-danger">
-          release date
-        </a>
-      </li>
-      <li className={styles.active}>
-        <a href="#byrate" className="text-muted">
-          rating
-        </a>
-      </li>
-    </ul>
-  </div>
-);
+class SortBy extends Component {
+  constructor() {
+    super();
+    this.state = {
+      sortBy: 'rate',
+    };
+  }
+
+  handelSort(e, val) {
+    e.preventDefault();
+    switch (val) {
+      case 'rate': this.setState({ sortBy: val });
+        break;
+      case 'release': this.setState({ sortBy: val });
+        break;
+      default: this.setState({ sortBy: 'rate' });
+    }
+  }
+
+  render() {
+    return (
+      <div className={styles.filters}>
+        <p className={styles.title}>Sort by: </p>
+        <ul className={styles.filtersList}>
+          <li className={this.state.sortBy === 'release' ? styles.active : null}>
+            <a href="" onClick={e => this.handelSort(e, 'release')}>
+              release date
+            </a>
+          </li>
+          <li className={this.state.sortBy === 'rate' ? styles.active : null}>
+            <a href="" onClick={e => this.handelSort(e, 'rate')}>
+              rating
+            </a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default SortBy;
