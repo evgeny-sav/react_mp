57fadb45f76d3b283ae72c38f5bab7f844e07e43
Yauheni_Sauchanka <Yauheni_Sauchanka@epam.com>
2017-09-26 16:04:39 +0300

    routes fixed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { Route, Switch } from 'react-router-dom';
 import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+import MovieDetails from '../MovieDetails/MovieDetails';
+import SearchBar from '../SearchBar/SearchBar';
+import InfoBar from '../InfoBar/InfoBar';
+import SortBy from '../SortBy/SortBy';
+import Main from '../Main/Main';
 import styles from './App.scss';
 
-const App = props => (
+const App = () => (
   <div className={styles.outer}>
     <div className={styles.wrapper}>
-      { props.children }
+      <Header>
+        <Switch>
+          <Route path="/film/:id" component={MovieDetails} />
+          <Route path="*" component={SearchBar} />
+        </Switch>
+      </Header>
+      <InfoBar>
+        <Route path="/" component={SortBy} />
+      </InfoBar>
+      <Main />
       <div className={styles.push}>&nbsp;</div>
     </div>
     <Footer />
   </div>
 );
-
-App.propTypes = {
-  children: PropTypes.element.isRequired,
-};
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,41 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/App/App';
-import Header from './components/Header/Header';
-import MovieDetails from './components/MovieDetails/MovieDetails';
-import SearchBar from './components/SearchBar/SearchBar';
-import InfoBar from './components/InfoBar/InfoBar';
-import SortBy from './components/SortBy/SortBy';
-import Main from './components/Main/Main';
-import NotFound from './components/NotFound/NotFound';
 import './index.scss';
 
-function renderRoutes() {
-  return (
-    <Router>
-      <App>
-        <Switch>
-          <Route path="/">
-            <div>
-              <Header>
-                <Switch>
-                  <Route path="/" component={SearchBar} exact />
-                  <Route path="/search" component={SearchBar} />
-                  <Route path="/film/:id" component={MovieDetails} />
-                </Switch>
-              </Header>
-              <InfoBar>
-                <Route path="/" component={SortBy} />
-              </InfoBar>
-              <Main />
-            </div>
-          </Route>
-          <Route path="*" component={NotFound} />
-        </Switch>
-      </App>
-    </Router>
-  );
-}
+const renderRoutes = () => <Router><App /></Router>;
 
 ReactDOM.render(renderRoutes(), document.getElementById('root'));
