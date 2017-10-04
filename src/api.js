5d1da5bc546f367c78d6c55ef04d7bdd9d38be8c
Yauheni_Sauchanka <Yauheni_Sauchanka@epam.com>
2017-10-04 12:08:06 +0300

    small fixes, netflixroulette api url generation

--- src/api.js
+++ src/api.js
@@ -5,7 +5,9 @@ export default class API {
     return Axios.get('http://localhost:3000/api/v1/movies');
   }
 
-  static getData() {
+  static getData(searchBy, searchFor) {
+    const url = `http://netflixroulette.net/api/api.php?${searchBy}=${searchFor}`;
+    console.log(`[netflixroulette]: ${url}`);
     return Axios.get('http://localhost:3000/api/v1/movies');
   }
 }
